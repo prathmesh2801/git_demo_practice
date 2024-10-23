f34436f88e747916832db5ac1861b31302c62c8d
prathmesh2801 <prathmesh280199@gmail.com>
2024-10-23 11:12:08 +0530

    str method add in models.py

--- git_demo/demo_app/models.py
+++ git_demo/demo_app/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class userinfo(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.name}"
+    
